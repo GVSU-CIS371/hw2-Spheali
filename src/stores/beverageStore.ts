5169e857213610a8b797156fc17147e5a339ba8a
Spheali <cowan.gavin04@gmail.com>
2025-04-02 20:27:34 -0400

    Added User State Beverages

--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -3,22 +3,41 @@ import tempretures from "../data/tempretures.json";
 import bases from "../data/bases.json";
 import creamers from "../data/creamers.json";
 import syrups from "../data/syrups.json";
+import { BaseBeverageType, CreamerType, SyrupType, BeverageType } from "../types/beverage.ts"
+
+interface BeverageState {
+  temps: string[],
+  currentTemp: string,
+  bases: BaseBeverageType[],
+  currentBase: BaseBeverageType,
+  creamers: CreamerType[],
+  currentCream: CreamerType,
+  syrups: SyrupType[],
+  currentSyrup: SyrupType,
+  userBev: BeverageType[]|null,
+  currentBev: BeverageType|null
+}
 
 export const useBeverageStore = defineStore("BeverageStore", {
   state: () => ({
     temps: tempretures,
     currentTemp: tempretures[0],
     bases: bases,
-    currentBase: bases[2],
+    currentBase: bases[0],
     creamers: creamers,
-    currentCream: creamers[1],
+    currentCream: creamers[0],
     syrups: syrups,
-    currentSyrup: syrups[1]
+    currentSyrup: syrups[0],
+    userBev: null,
+    currentBev: null
   }),
 
   actions: {
-    makeBeverage() {},
-    showBeverage() {},
+    makeBeverage() {
+      
+    },
+    showBeverage() {
+
+    },
   },
-  persist: true,
 });
